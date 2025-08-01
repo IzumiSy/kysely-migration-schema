537207f83099374f43fbf569da88e8ae41ae70c4
IzumiSy <beetle-noise@gmx.com>
2025-08-01 21:32:13 +0900

    Add assert function

--- packages/cli/src/applyDiff.ts
+++ packages/cli/src/applyDiff.ts
@@ -1,4 +1,4 @@
-import { Kysely } from "kysely";
+import { ColumnDataType, isColumnDataType, Kysely } from "kysely";
 import { TableDiff } from "./diff";
 
 /**
@@ -14,8 +14,10 @@ export async function applyDiff(
   for (const added of diff.addedTables) {
     let builder = db.schema.createTable(added.table);
     for (const [colName, colDef] of Object.entries(added.columns)) {
-      // 最低限 typeのみ対応（拡張は後で）
-      builder = builder.addColumn(colName, colDef.type as any);
+      const dataType = colDef.type;
+      assertDataType(dataType);
+      // 最低限typeのみ対応（拡張は後で）
+      builder = builder.addColumn(colName, dataType);
     }
     await builder.execute();
   }
@@ -29,9 +31,11 @@ export async function applyDiff(
   for (const changed of diff.changedTables) {
     // 追加カラム
     for (const addCol of changed.addedColumns) {
+      const dataType = addCol.definition.type;
+      assertDataType(dataType);
       await db.schema
         .alterTable(changed.table)
-        .addColumn(addCol.column, addCol.definition.type as any)
+        .addColumn(addCol.column, dataType)
         .execute();
     }
     // 削除カラム
@@ -45,10 +49,20 @@ export async function applyDiff(
     for (const chCol of changed.changedColumns) {
       await db.schema
         .alterTable(changed.table)
-        .alterColumn(chCol.column, (col) =>
-          col.setDataType(chCol.after.type as any)
-        )
+        .alterColumn(chCol.column, (col) => {
+          const dataType = chCol.after.type;
+          assertDataType(dataType);
+          return col.setDataType(dataType);
+        })
         .execute();
     }
   }
 }
+
+const assertDataType: (
+  dataType: string
+) => asserts dataType is ColumnDataType = (dataType) => {
+  if (!isColumnDataType(dataType)) {
+    throw new Error(`Unsupported data type: ${dataType}`);
+  }
+};
